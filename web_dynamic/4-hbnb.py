3fc9eeed10145d1caaa3fc7b7d653b70b818efd7
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-12 17:39:37 +0300

    HBNB Project

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" the Flask Web Application Entry Point: the HBNB"""
 from models import storage
 from models.state import State
 from models.city import City
@@ -8,40 +8,47 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
+# Creating the Flask application instance
+app = Flask(__name__)
+
+# Optional: Modify Jinja2 template handling for whitespace
+# app.jinja_env.trim_blocks = True  # Trims blocks in templates
+# app.jinja_env.lstrip_blocks = True  # Strips leading spaces from blocks
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """Tearing down the present SQLAlchemy Session"""
     storage.close()
-
 
 @app.route('/4-hbnb', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """Rendering main HBNB page using dynamic data"""
+    
+    # Retrieveing all state objects, and sorting by name in alphabetic order
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
+    
+    # Preparing the list of states paired with the respective sorted cities
     st_ct = []
-
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
+    # Retrieving and sorting by name all the Amenity objects 
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
 
+    # Retrieving and sorting by name the Place objects 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
+    # Rendering '4-hbnb.html' template, pass sorted data
     return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=uuid.uuid4())
 
-
 if __name__ == "__main__":
-    """ Main Function """
+    """Starting Flask web application"""
     app.run(host='0.0.0.0', port=5000)
