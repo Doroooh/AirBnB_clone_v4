654a7793ab1dc04dc448564fad7a8efca8177291
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-12 21:47:42 +0300

    HBNB Project

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -1,51 +1,68 @@
+// Accessing jQuery from global window object
 const $ = window.jQuery;
 
+// Document ready function that ensures DOM is fully loaded before script execution
 $(document).ready(function () {
+  // Initializing objects for storing selected amenities, states, and cities
   const amenities = {};
   const states = {};
   const cities = {};
+
+  // Event handler to click on amenity checkboxes
   $('input[type="checkbox"].amen-checkbox').click(function () {
     $(this).each(function () {
+      // Adding or removing an amenity 
       if (this.checked) {
         amenities[$(this).data('id')] = $(this).data('name');
       } else {
         delete amenities[$(this).data('id')];
       }
     });
+    // Updating amenities list for display
     $('.amenities h4').html(Object.values(amenities).join(', ') || '&nbsp;');
   });
 
+  // click on state checkboxes
   $('input[type="checkbox"].state-checkbox').click(function () {
     $(this).each(function () {
+      // Add or remove state 
       if (this.checked) {
         states[$(this).data('id')] = $(this).data('name');
       } else {
         delete states[$(this).data('id')];
       }
     });
+    // Update locations list 
     const locations = Object.values(states).concat(Object.values(cities));
     $('.locations h4').html(locations.join(', ') || '&nbsp;');
   });
 
+  // click city checkboxes
   $('input[type="checkbox"].city-checkbox').click(function () {
     $(this).each(function () {
+      // Add or remove city 
       if (this.checked) {
         cities[$(this).data('id')] = $(this).data('name');
       } else {
         delete cities[$(this).data('id')];
       }
     });
+    // Update locations list 
     const locations = Object.values(states).concat(Object.values(cities));
     $('.locations h4').html(locations.join(', ') || '&nbsp;');
   });
 
+  // click the search button
   $('button').click(() => {
+    // Collect filters on the object
     const data = {
       amenities: Object.keys(amenities),
       states: Object.keys(states),
       cities: Object.keys(cities)
     };
+    // clear places container before loading new data
     $('.places').empty();
+    // Perform AJAX POST request that will fetch filtered places
     $.ajax({
       method: 'POST',
       url: 'http://localhost:5001/api/v1/places_search/',
@@ -53,26 +70,32 @@ $(document).ready(function () {
       contentType: 'application/json; charset=utf-8'
     })
       .done(function (data, status) {
+        // handle received places data
         findPlaces(data);
       });
   });
 });
 
+// Perform GET request that check API status
 $.get('http://localhost:5001/api/v1/status/')
   .done(function (data, status) {
+    // Update API status indicator
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
     }
   })
   .fail(function () {
+    // Remove 'available' class if request fails
     $('#api_status').removeClass('available');
   });
 
+// Function that retrieves and formats reviews
 async function getReviews (reviews) {
   let reviewTemplate;
   let reviewsUL = '';
   let user;
   let formatDate;
+  // Looping review and format
   for (const review of reviews) {
     user = await $.get(`http://localhost:5001/api/v1/users/${review.user_id}`);
     formatDate = new Date(review.updated_at).toLocaleDateString(
@@ -87,11 +110,13 @@ async function getReviews (reviews) {
   return reviewsUL;
 }
 
+// Function to find and display places based on search results
 async function findPlaces (places) {
   let placeTemplate;
   let amenities;
   let amenityTemplate;
   let reviews;
+  // Looping through place and create its template
   for (const place of places) {
     amenities = await $.get(`http://localhost:5001/api/v1/places/${place.id}/amenities`);
     reviews = await $.get(`http://localhost:5001/api/v1/places/${place.id}/reviews`);
@@ -106,7 +131,7 @@ async function findPlaces (places) {
         <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
         <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
       </div>
-      <!-- user div removed -->
+      <!-- User div has been removed -->
       <div class="description">
         ${place.description}
       </div>
@@ -124,17 +149,21 @@ async function findPlaces (places) {
       </div>
     </article>
     `;
+    // Append the place template to the places container
     $('.places').append(placeTemplate);
+    // Add amenities to the place's amenities section
     for (const amenity of amenities) {
       amenityTemplate = `
         <li class="${amenity.name.toLowerCase().replace(' ', '_')}">${amenity.name}</li>
         `;
       $('.amenities:last ul').append(amenityTemplate);
     }
+    // Add reviews to the place's reviews section
     $('.reviews:last ul').append(await getReviews(reviews));
   }
 }
 
+// Initial AJAX request to fetch all places and display them
 $.ajax({
   method: 'POST',
   url: 'http://localhost:5001/api/v1/places_search/',
@@ -142,8 +171,10 @@ $.ajax({
   contentType: 'application/json; charset=utf-8'
 })
   .done(function (data, status) {
+    // Call function that handle and displays the fetched places
     findPlaces(data);
   })
   .fail(function () {
+    // Hiding places container if the request fails
     $('.places').hide();
   });
